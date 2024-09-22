fd6559f7876ffaa444cee753e8c8fa060b0de642
Luis Martinez <lmartin1@stu.southtexascollege.edu>
2024-09-22 15:10:13 -0500

    finished ItemsOnHand component

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import Header from './components/Header.vue';
+  import ItemsOnHand from './components/ItemsOnHand.vue';
+
+</script>
+
+<template>
+  <Header></Header>
+  <ItemsOnHand></ItemsOnHand>
+
+
+
+
+
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+
+</script>
+
+<template>
+    <h2>Inventory Tracker</h2>
+
+
+
+
+</template>
