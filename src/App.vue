9691f2b8e1d180e83bb44c127a67f16496215476
Luis Martinez <lmartin1@stu.southtexascollege.edu>
2024-09-22 15:41:20 -0500

    final product

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <script setup>
-  import Header from './components/Header.vue';
-  import ItemsOnHand from './components/ItemsOnHand.vue';
-  import ItemsRequired from './components/ItemsRequired.vue';
-  import ItemsToRequest from './components/ItemsToRequest.vue';
+import { ref, onMounted } from 'vue';
+import Header from './components/Header.vue';
+import ItemsOnHand from './components/ItemsOnHand.vue';
+import ItemsRequired from './components/ItemsRequired.vue';
+import ItemsToRequest from './components/ItemsToRequest.vue';
 
+const items = ref([]);
+
+const saveToLocalStorage = () => {
+  localStorage.setItem('inventoryItems', JSON.stringify(items.value));
+};
+
+onMounted(() => {
+  const storedItems = localStorage.getItem('inventoryItems');
+  if (storedItems) {
+    items.value = JSON.parse(storedItems);
+  }
+});
+
+defineExpose({ items, saveToLocalStorage });
 </script>
 
 <template>
   <Header></Header>
-  <ItemsOnHand></ItemsOnHand>
+  <ItemsOnHand :items="items" @save="saveToLocalStorage"></ItemsOnHand>
   <ItemsRequired></ItemsRequired>
   <ItemsToRequest></ItemsToRequest>
-
-
-
-
 
 </template>
--- src/components/ItemsOnHand.vue
+++ src/components/ItemsOnHand.vue
@@ -1,46 +1,60 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+
+const props = defineProps(['items']);
+const emit = defineEmits();
 
 const newItemName = ref('');
 const newItemQuantity = ref(1);
-const items = ref([]);
 
 const addItem = () => {
   if (newItemName.value.trim() && newItemQuantity.value > 0) {
-    items.value.push({
+    props.items.push({
       name: newItemName.value.trim(),
       quantity: newItemQuantity.value,
     });
     newItemName.value = '';
-    newItemQuantity.value = 1; // Reset quantity to default
+    newItemQuantity.value = 1; 
+
+    emit('save');
   }
 };
+
+const deleteItem = (index) => {
+  props.items.splice(index, 1);
+  emit('save');
+};
+
+watch(() => props.items, () => {
+  emit('save');
+}, { deep: true });
 </script>
 
 <template>
-    <div class="inventory-tracker">
-      <h1>Inventory Tracker</h1>
-      
-      <input
-        type="text"
-        v-model="newItemName"
-        placeholder="Enter item name"
-        @keyup.enter="addItem"
-      />
-      
-      <input
-        type="number"
-        v-model="newItemQuantity"
-        placeholder="Enter quantity"
-        @keyup.enter="addItem"
-      />
-      
-      <button @click="addItem">Add Item</button>
-      
-      <ul>
-        <li v-for="(item, index) in items" :key="index">
-          {{ item.name }} - {{ item.quantity }}
-        </li>
-      </ul>
-    </div>
-  </template>
+  <div class="items-on-hand">
+    <h1>Items On Hand</h1>
+    
+    <input
+      type="text"
+      v-model="newItemName"
+      placeholder="Enter item name"
+      @keyup.enter="addItem"
+    />
+    
+    <input
+      type="number"
+      v-model="newItemQuantity"
+      placeholder="Enter quantity"
+      @keyup.enter="addItem"
+    />
+    
+    <button @click="addItem">Add Item</button>
+
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item.name }} - {{ item.quantity }}
+        <button @click="deleteItem(index)">Delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
